d9a384ebfe2e51b4c0407290a8025ba9c9af268f
Alexander <leonxnadercs@gmail.com>
2020-12-09 13:54:36 -0500

    remove dist

--- dist/src/app/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/// <reference types="es4x" />
-// @ts-check
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./verticle/app");
-const application = new app_1.Application();
-application.run();
--- dist/src/infrastructure/mongo/enums.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-module.exports = {
-    BulkOperationType: Java.type('io.vertx.ext.mongo.BulkOperationType'),
-    WriteOption: Java.type('io.vertx.ext.mongo.WriteOption')
-};
